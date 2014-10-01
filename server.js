a0a6a761ef7734bc1cbfc02c2a1ab85f77381a80
Olaf Radicke <briefkasten@olaf-radicke.de>
2014-10-01 20:29:39 +0200

    begin with database programming

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var AppRoutes  = require('./app/definitions/routing.js');
 
+
 app.set('views', __dirname + "/app/views/");
 app.set('view engine', 'jade');
 
@@ -12,5 +13,7 @@ AppRoutes.init( app );
 
 
 var server = app.listen(3000, function() {
+    global.global_vars = {bekannt: "ja"};
     console.log('Listening on port %d', server.address().port);
+
 });
