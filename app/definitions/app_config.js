a7fe5a9d0d1ba840479958c52f5f8f7bc3a48fc3
Olaf Radicke <briefkasten@olaf-radicke.de>
2014-10-04 08:22:53 +0200

    Add missing file.

---
+++ app/definitions/app_config.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var AppConfig = function AppConfig(){};
+
+AppConfig.prototype.db = {
+    host: "localhost",
+    port: "9999"
+};
+
+AppConfig.prototype.filename = "config-file.json"
+
+var app_config = new AppConfig();
+console.log('[app_config] filename ' + app_config.filename);
+console.log('[app_config] host ' + app_config.db.host);
+
+module.exports = app_config;
+
+
