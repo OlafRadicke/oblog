05072837e0d203e9a51e34a476353b45be886b91
Olaf Radicke <briefkasten@olaf-radicke.de>
2014-10-14 09:35:55 +0200

    Continus with table init function.

--- app/definitions/db_init.js
+++ app/definitions/db_init.js
@@ -1,6 +1,6 @@
 "use strict";
-var app_config = require('../definitions/app_config.js');
 
+var app_config = require('../definitions/app_config.js');
 var knex = require('knex')({
     client: app_config.db.client,
     connection: {
@@ -8,6 +8,13 @@ var knex = require('knex')({
         debug: app_config.db.debug
     }
 });
+var crypto = require('crypto');
+
+function randomValueHex (len) {
+    return crypto.randomBytes(Math.ceil(len/2))
+        .toString('hex') // convert to hexadecimal format
+        .slice(0,len);   // return required number of characters
+}
 
 var db_init = {
     check_exist: function(){
@@ -53,7 +60,7 @@ var db_init = {
                 return knex.schema.createTable('user', function(table) {
                   table.increments('id').primary();
                   table.integer('email').notNullable();
-                  table.text('name').defaultTo("") ;
+                  table.text('name').unique()  ;
                   table.text('salt').notNullable() ;
                   table.text('pw_hash').notNullable() ;
                 }).debug();
@@ -83,10 +90,51 @@ var db_init = {
         });
 
         knex('schema_version')
+            .select('version_number')
+            .debug()
+            .then( function(rows) {
+                console.log( '[db_init] The version is: ' + rows[0].version_number );
+            }).catch(function(error) {
+//                 console.error(error);
+                // Wahrscheinlich wird die DB zum ersten mal angelegt.
+
+                var pw_salt = randomValueHex (16);
+                console.log('[db_init] pw_salt: ' + pw_salt );
+
+                var pw_hash = crypto
+                    .createHash('sha1')
+                    .update( 'oblog' + pw_hash, 'utf8')
+                    .digest( 'hex');
+                console.log('[db_init] pw_hash: ' + pw_hash );
+
+
+                knex('user')
+                    .insert( {
+                        email: 'set@you-address.now',
+                        name: 'oblog',
+                        salt: pw_salt,
+                        pw_hash: pw_hash
+                    })
+                    .debug()
+                    .then(function(inserts) {
+                        console.log('[db_init] ' + inserts.length + ' new version saved.');
+                    });
+            });
+
+
+        knex('schema_version')
+            .where('version_number', 1)
+            .del()
+            .debug()
+            .then(function(inserts) {
+                console.log('[db_init] ' + inserts.length + ' delete old version number.');
+            });
+
+        knex('schema_version')
             .insert( {version_number: 1, update: Date.now()} )
             .debug()
             .then(function(inserts) {
-                console.log(inserts.length + ' new version saved.');
+                console.log('[db_init] ' + inserts.length + ' new version saved.');
             });
     },
     check_update: function(){
