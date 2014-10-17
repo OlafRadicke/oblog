36b1e8da0497e98f88b6e2ed3b7a80cf2b99d91d
Olaf Radicke <briefkasten@olaf-radicke.de>
2014-10-17 12:10:04 +0200

    Remove async

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var async = require('async');
 var app_config = require('../definitions/app_config.js');
 var crypto = require('crypto');
 var knex = require('knex')({
@@ -29,29 +28,16 @@ module.exports = {
         console.log('[login] Name: ' + req.body.name);
         console.log('[login] Password: ' + req.body.password);
 
-        async.series(
-            [
-                function( cbf){
 
-                    knex('user')
-                        .select('salt','pw_hash')
-                        .where({name: req.body.name})
-                        .debug()
-                        .then( function(rows) {
-                            console.log( '[login] salt: ' + rows[0].salt );
-                            console.log( '[login] pw_hash: ' + rows[0].pw_hash );
-                            db_salt = rows[0].salt;
-                            db_pw_hash = rows[0].pw_hash;
-                            cbf();
-                        }).catch(function(error) {
-                            console.log( '[login] crash!' + error );
-                            flash_message_error = "Loginname oder Passwort falsch!";
-                            cbf();
-                        });
-                }
-            ],
-            // optional callback
-            function(err){
+        knex('user')
+            .select('salt','pw_hash')
+            .where({name: req.body.name})
+            .debug()
+            .then( function(rows) {
+                console.log( '[login] salt: ' + rows[0].salt );
+                console.log( '[login] pw_hash: ' + rows[0].pw_hash );
+                db_salt = rows[0].salt;
+                db_pw_hash = rows[0].pw_hash;
 
                 console.log( '[login] db_salt: ' + db_salt );
                 console.log( '[login] db_pw_hash: ' + db_pw_hash );
@@ -67,7 +53,13 @@ module.exports = {
                 };
 
                 res.render('login.jade', {  flash_message: flash_message, flash_message_error: flash_message_error  });
-            }
-        );
+
+            }).catch(function(error) {
+                console.log( '[login] crash!' + error );
+                flash_message_error = "Loginname oder Passwort falsch!";
+                res.render('login.jade', {  flash_message: flash_message, flash_message_error: flash_message_error  });
+            });
+
+
     }
 }
