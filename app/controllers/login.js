fd919e931a9c86857f849b5d82151043f486a720
Olaf Radicke <briefkasten@olaf-radicke.de>
2014-11-11 20:52:37 +0100

    acl debugging

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -67,24 +67,30 @@ module.exports = {
 
                             knex('user_role')
                                 .select('role_name')
-                                .where({ id: id_rows[0].id  })
+                                .where({ user_id: id_rows[0].id  })
                                 .debug()
                                 .then( function(role_rows) {
                                     console.log( '[login] serch role_rows: ' + JSON.stringify(role_rows) );
 
                                     for (var i = 0, len = role_rows.length; i < len; i++) {
-                                        console.log( '[login] push: ' + role_rows[0].role_name );
+                                        console.log( '[login] push: ' + role_rows[i].role_name );
+                                        console.log( '[login] runde: ' + i );
+                                        session_data.rolls[i] = role_rows[i].role_name;
+                                        console.log( '[login] all roles: ' + JSON.stringify(session_data.rolls) );
 
-                                        session_data.rolls.push(role_rows[0].role_name);
                                         // is all added
-                                        if (i == len - 1) {
-                                            console.log( '[login] rolls: ' + req.body.rolls );
-                                            res.render('login_okay.jade', {
-                                                rolls: session_data.rolls,
-                                                user_name: req.body.name
-                                            });
-                                        }
+//                                         if (i == len - 1) {
+//                                             console.log( '[login] rolls: ' + JSON.stringify(session_data.rolls) );
+//                                             res.render('login_okay.jade', {
+//                                                 rolls: session_data.rolls,
+//                                                 user_name: req.body.name
+//                                             });
+//                                         }
                                     }
+                                    res.render('login_okay.jade', {
+                                        rolls: session_data.rolls,
+                                        user_name: req.body.name
+                                    });
 
                                 });
                         });
